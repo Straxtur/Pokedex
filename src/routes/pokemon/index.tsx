c6ee2b743b3e8aac2ecc8ae14f8795f094660c69
Jorge Sánchez López <jorgeraton70@gmail.com>
2025-01-04 11:51:23 +0100

    fix(Pokedex): corregir sintaxis en la declaración del componente

--- src/routes/pokemon/index.tsx
+++ src/routes/pokemon/index.tsx
@@ -6,7 +6,7 @@ import { createFileRoute } from "@tanstack/react-router";
 export const Route = createFileRoute("/pokemon/")({
 	component: () => (
 		<CurrentPageProvider>
-			<Pokedex />,
+			<Pokedex />
 		</CurrentPageProvider>
 	),
 });
